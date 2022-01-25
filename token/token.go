cf68d6f5aa2fdf24ecace4b40b7d844d335916bc
Tom <tomfleet2018@gmail.com>
2022-01-25 21:57:48 +0000

    Pretty print tokens

--- token/token.go
+++ token/token.go
@@ -2,23 +2,47 @@
 // as well as basic operations on those tokens e.g. printing
 package token
 
-// Token is the set of lexical tokens in spok
-type Token int
+import "fmt"
 
-//go:generate stringer -type=Token -linecomment
+// Type is the set of lexical tokens in spok
+type Type int
+
+//go:generate stringer -type=Type -linecomment -output=token_string.go
 const (
-	ERROR    Token = iota // ERROR
-	EOF                   // EOF
-	COMMENT               // COMMENT
-	LPAREN                // (
-	RPAREN                // )
-	LBRACE                // {
-	RBRACE                // }
-	LBRACKET              // [
-	RBRACKET              // ]
-	QUOTE                 // "
-	TASK                  // TASK
-	STRING                // STRING
-	OUTPUT                // ->
-	IDENT                 // IDENT
+	ERROR    Type = iota // ERROR
+	EOF                  // EOF
+	COMMENT              // COMMENT
+	LPAREN               // (
+	RPAREN               // )
+	LBRACE               // {
+	RBRACE               // }
+	LBRACKET             // [
+	RBRACKET             // ]
+	QUOTE                // "
+	TASK                 // TASK
+	STRING               // STRING
+	OUTPUT               // ->
+	IDENT                // IDENT
+	DECLARE              // :=
 )
+
+// Token represents a spok lexical token
+type Token struct {
+	Value string // Value, e.g. "("
+	Type  Type   // Type, e.g. LPAREN
+	Pos   int    // Starting position of this token in the input string
+	Line  int    // Line number at the start of this token
+}
+
+// String satisfies the stringer interface and allows us to pretty print the tokens
+func (t Token) String() string {
+	switch {
+	case t.Type == EOF:
+		return "EOF"
+	case t.Type == ERROR:
+		return t.Value
+	case len(t.Value) > 15:
+		return fmt.Sprintf("%.15q...", t.Value)
+	}
+	return fmt.Sprintf("%q", t.Value)
+}
