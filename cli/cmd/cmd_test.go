8063e3cc94de835c3e78ad73a4177454654f8b8c
Tom <tomfleet2018@gmail.com>
2022-01-28 21:58:42 +0000

    Tweak comment in cmd

--- cli/cmd/cmd_test.go
+++ cli/cmd/cmd_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 // Completely pointless test, but my OCD was being triggered as this is the
 // only package that isn't green when running gotest.
+// The only thing that happens in this package is cobra stuff anyway.
 func TestPointless(t *testing.T) {
 	got := 1 + 1
 	want := 2
