07f5bdcb693e278be38a2adf6fdf4a122d72c693
Tom <tomfleet2018@gmail.com>
2022-01-28 18:21:04 +0000

    Add some smoke tests to App and CLI

---
+++ cli/app/app_test.go
@@ -0,0 +1,62 @@
+package app
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func TestApp_Run(t *testing.T) {
+	type fields struct {
+		Out     io.Writer
+		Options *Options
+	}
+	type args struct {
+		tasks []string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "just tasks",
+			fields: fields{
+				Out:     &bytes.Buffer{},
+				Options: &Options{},
+			},
+			args:    args{tasks: []string{"task1", "task2", "task3"}},
+			wantErr: false,
+		},
+		{
+			name: "no tasks",
+			fields: fields{
+				Out:     &bytes.Buffer{},
+				Options: &Options{},
+			},
+			args:    args{tasks: []string{}},
+			wantErr: false,
+		},
+		{
+			name: "show",
+			fields: fields{
+				Out:     &bytes.Buffer{},
+				Options: &Options{Show: "task1"},
+			},
+			args:    args{tasks: []string{}},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &App{
+				Out:     tt.fields.Out,
+				Options: tt.fields.Options,
+			}
+			if err := a.Run(tt.args.tasks); (err != nil) != tt.wantErr {
+				t.Errorf("App.Run() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
