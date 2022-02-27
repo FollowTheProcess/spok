1abc3e321e5bed5794950cc5370a0f1e27ec4dfc
Tom <tomfleet2018@gmail.com>
2022-02-27 11:26:32 +0000

    Ensure expanded globs are made absolute

--- task/task.go
+++ task/task.go
@@ -4,19 +4,32 @@ package task
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/FollowTheProcess/spok/ast"
 	"github.com/bmatcuk/doublestar/v4"
 )
 
-// expandGlob expands out the glob pattern from root and returns all the matches.
+// expandGlob expands out the glob pattern from root and returns all the matches,
+// the matches are made absolute before returning, root should be absolute.
 func expandGlob(root, pattern string) ([]string, error) {
 	matches, err := doublestar.Glob(os.DirFS(root), pattern)
 	if err != nil {
-		return nil, fmt.Errorf("could not expand glob pattern '%s': %w", pattern, err)
+		return nil, fmt.Errorf("could not expand glob pattern '%s': %w", filepath.Join(root, pattern), err)
 	}
-	return matches, nil
+
+	var absMatches []string
+	for _, match := range matches {
+		joined := filepath.Join(root, match)
+		abs, err := filepath.Abs(joined)
+		if err != nil {
+			return nil, fmt.Errorf("could not resolve path '%s' to absolute: %w", joined, err)
+		}
+		absMatches = append(absMatches, abs)
+	}
+
+	return absMatches, nil
 }
 
 // Task represents a spok Task.
@@ -39,12 +52,14 @@ func newTask(t ast.Task, root string) (Task, error) {
 		case dep.Type() == ast.NodeString:
 			// String means file dependency
 			if strings.Contains(dep.String(), "*") {
+				// We have a glob pattern
 				matches, err := expandGlob(root, dep.String())
 				if err != nil {
 					return Task{}, err
 				}
 				fileDeps = append(fileDeps, matches...)
 			} else {
+				// We have something like "file.go"
 				fileDeps = append(fileDeps, dep.String())
 			}
 		case dep.Type() == ast.NodeIdent:
--- task/task_test.go
+++ task/task_test.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -26,9 +27,13 @@ func TestExpandGlob(t *testing.T) {
 		t.Fatalf("expandGlob returned an error: %v", err)
 	}
 
-	want := []string{"sub1/sub2/blah.txt", "sub1/sub2/sub3/hello.txt", "suba/subb/stuff.txt", "suba/subb/subc/something.txt"}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %#v, wanted %#v", got, want)
+	want := []string{"top.txt", "sub1/sub2/blah.txt", "sub1/sub2/sub3/hello.txt", "suba/subb/stuff.txt", "suba/subb/subc/something.txt"}
+	var wantAbs []string
+	for _, w := range want {
+		wantAbs = append(wantAbs, mustAbs(root, w))
+	}
+	if !reflect.DeepEqual(got, wantAbs) {
+		t.Errorf("got %#v, wanted %#v", got, wantAbs)
 	}
 }
 
@@ -145,18 +150,91 @@ func TestNewTask(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "simple with double glob dependency",
+			want: Task{
+				Doc:              "A simple test task",
+				Name:             "simple",
+				TaskDependencies: nil,
+				FileDependencies: []string{
+					mustAbs(testdata, "top.txt"),
+					mustAbs(testdata, "sub1/sub2/blah.txt"),
+					mustAbs(testdata, "sub1/sub2/sub3/hello.txt"),
+					mustAbs(testdata, "suba/subb/stuff.txt"),
+					mustAbs(testdata, "suba/subb/subc/something.txt"),
+				},
+				Commands: []string{"go test ./..."},
+			},
+			in: ast.Task{
+				Name:         ast.Ident{Name: "simple", NodeType: ast.NodeIdent},
+				Docstring:    ast.Comment{Text: " A simple test task", NodeType: ast.NodeComment},
+				Dependencies: []ast.Node{ast.String{Text: "**/*.txt", NodeType: ast.NodeString}},
+				Outputs:      []ast.Node{},
+				Commands:     []ast.Command{{Command: "go test ./...", NodeType: ast.NodeCommand}},
+				NodeType:     ast.NodeTask,
+			},
+			wantErr: false,
+		},
+		{
+			name: "simple with single glob dependency",
+			want: Task{
+				Doc:              "A simple test task",
+				Name:             "simple",
+				TaskDependencies: nil,
+				FileDependencies: []string{mustAbs(testdata, "top.txt")},
+				Commands:         []string{"go test ./..."},
+			},
+			in: ast.Task{
+				Name:         ast.Ident{Name: "simple", NodeType: ast.NodeIdent},
+				Docstring:    ast.Comment{Text: " A simple test task", NodeType: ast.NodeComment},
+				Dependencies: []ast.Node{ast.String{Text: "*.txt", NodeType: ast.NodeString}},
+				Outputs:      []ast.Node{},
+				Commands:     []ast.Command{{Command: "go test ./...", NodeType: ast.NodeCommand}},
+				NodeType:     ast.NodeTask,
+			},
+			wantErr: false,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := newTask(tt.in, testdata)
+			got, err := newTask(tt.in, testdata) // Initialise root at testdata
 			if (err != nil) != tt.wantErr {
 				t.Fatalf("newTask() err = %v, wanted %v", err, tt.wantErr)
 			}
 
 			if diff := cmp.Diff(tt.want, got); diff != "" {
-				t.Errorf("Function mismatch (-want +got):\n%s", diff)
+				t.Errorf("Task mismatch (-want +got):\n%s", diff)
 			}
 		})
 	}
+}
+
+func BenchmarkExpandGlob(b *testing.B) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		b.Fatalf("could not get cwd: %v", err)
+	}
+
+	root, err := filepath.Abs(filepath.Join(cwd, "testdata"))
+	if err != nil {
+		b.Fatalf("could not resolve root: %v", err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err = expandGlob(root, "**/*.txt")
+		if err != nil {
+			b.Fatalf("expandGlob returned an error: %v", err)
+		}
+	}
+}
+
+// mustAbs returns the resolved 'path' or panics if it cannot.
+func mustAbs(root, path string) string {
+	abs, err := filepath.Abs(filepath.Join(root, path))
+	if err != nil {
+		panic(fmt.Sprintf("mustAbs could not resolve '%s'", filepath.Join(root, path)))
+	}
+	return abs
 }
