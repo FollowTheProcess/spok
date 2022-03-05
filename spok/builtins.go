af12f511f3f69d0b7899449dcd9cf1e888e66d2c
Tom <tomfleet2018@gmail.com>
2022-03-05 09:49:38 +0000

    Refactor the join builtin

--- spok/builtins.go
+++ spok/builtins.go
@@ -2,8 +2,15 @@ package spok
 
 import "path/filepath"
 
+// builtin is a spok built in function.
 type builtin func(...string) (string, error)
 
+// package scoped map mapping the names of the builtins to their underlying function.
 var builtins = map[string]builtin{
-	"join": func(parts ...string) (string, error) { return filepath.Join(parts...), nil },
+	"join": join,
+}
+
+// join joins up filepath parts with an OS specific separator.
+func join(parts ...string) (string, error) {
+	return filepath.Join(parts...), nil
 }
