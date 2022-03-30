6adea5a8bd1515b42196522be92239e0c86aa262
Tom <tomfleet2018@gmail.com>
2022-03-30 12:37:09 +0100

    First implementation of a DAG topological sort

--- graph/graph.go
+++ graph/graph.go
@@ -5,53 +5,46 @@ package graph
 import (
 	"fmt"
 
+	"github.com/FollowTheProcess/collections/queue"
 	"github.com/FollowTheProcess/collections/set"
 )
 
 // Vertex represents a single node in the graph.
 type Vertex struct {
-	parents  *set.Set[Vertex] // The direct parents of this vertex
-	children *set.Set[Vertex] // The direct children of this vertex
-	Name     string           // Uniquely identifiable name
+	parents  *set.Set[*Vertex] // The direct parents of this vertex
+	children *set.Set[*Vertex] // The direct children of this vertex
+	Name     string            // Uniquely identifiable name
+	InDegree int               // Number of incoming edges
 }
 
 // NewVertex creates and returns a new Vertex.
-func NewVertex(name string) Vertex {
-	return Vertex{
-		parents:  set.New[Vertex](),
-		children: set.New[Vertex](),
+func NewVertex(name string) *Vertex {
+	return &Vertex{
+		parents:  set.New[*Vertex](),
+		children: set.New[*Vertex](),
 		Name:     name,
+		InDegree: 0,
 	}
-}
-
-// InDegree returns the number of incoming edges to this vertex.
-func (v Vertex) InDegree() int {
-	return v.parents.Length()
-}
-
-// OutDegree returns the number of outgoing edges to this vertex.
-func (v Vertex) OutDegree() int {
-	return v.children.Length()
 }
 
 // Graph is a DAG designed to hold spok tasks.
 type Graph struct {
-	vertices map[string]Vertex // Map of vertex name to vertex
+	vertices map[string]*Vertex // Map of vertex name to vertex
 }
 
 // New constructs and returns a new Graph.
 func New() *Graph {
-	return &Graph{vertices: make(map[string]Vertex)}
+	return &Graph{vertices: make(map[string]*Vertex)}
 }
 
 // AddVertex adds the passed vertex to the graph, if a vertex
 // with that name already exists it will be overwritten.
-func (g *Graph) AddVertex(v Vertex) {
+func (g *Graph) AddVertex(v *Vertex) {
 	g.vertices[v.Name] = v
 }
 
 // AddEdge creates an edge connection from parent to child vertices.
-func (g *Graph) AddEdge(parent, child Vertex) error {
+func (g *Graph) AddEdge(parent, child *Vertex) error {
 	parentVertex, ok := g.vertices[parent.Name]
 	if !ok {
 		return fmt.Errorf("parent vertex %q not in graph", parent.Name)
@@ -66,4 +59,48 @@ func (g *Graph) AddEdge(parent, child Vertex) error {
 	childVertex.parents.Add(parent)
 
 	return nil
+}
+
+// Sort topologically sorts the graph and returns a vertex slice in the correct order.
+func (g *Graph) Sort() ([]*Vertex, error) {
+	zeroInDegreeQueue := queue.New[*Vertex]()
+	result := make([]*Vertex, 0, len(g.vertices))
+
+	for _, vertex := range g.vertices {
+		vertex.InDegree = vertex.parents.Length() // Compute in degree for each vertex
+
+		// Put all vertices with 0 in-degree into the queue
+		if vertex.InDegree == 0 {
+			zeroInDegreeQueue.Push(vertex)
+		}
+	}
+
+	// Bailout point: if there is not at least 1 vertex with 0 in-degree
+	// it's not a DAG and cannot be sorted
+	if zeroInDegreeQueue.IsEmpty() {
+		return nil, fmt.Errorf("not a DAG")
+	}
+
+	// While queue is not empty
+	for !zeroInDegreeQueue.IsEmpty() {
+		vertex, err := zeroInDegreeQueue.Pop() // Pop a vertex off the queue
+		if err != nil {
+			return nil, err
+		}
+
+		// Add it to the result slice
+		result = append(result, vertex)
+
+		// For each child, reduce in-degree by 1
+		for _, child := range vertex.children.Items() {
+			child.InDegree--
+
+			// If any are now 0, add to the queue
+			if child.InDegree == 0 {
+				zeroInDegreeQueue.Push(child)
+			}
+		}
+	}
+
+	return result, nil
 }
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -74,3 +74,168 @@ func TestGraph_AddEdge(t *testing.T) {
 		}
 	})
 }
+
+func TestSort(t *testing.T) {
+	graph := New()
+
+	v1 := NewVertex("v1")
+	v2 := NewVertex("v2")
+	v3 := NewVertex("v3")
+	v4 := NewVertex("v4")
+	v5 := NewVertex("v5")
+
+	graph.AddVertex(v1)
+	graph.AddVertex(v2)
+	graph.AddVertex(v3)
+	graph.AddVertex(v4)
+	graph.AddVertex(v5)
+
+	// v2 depends on v1
+	if err := graph.AddEdge(v1, v2); err != nil {
+		t.Fatalf("graph.AddEdge returned an error: %v", err)
+	}
+
+	// v4 depends on v3
+	if err := graph.AddEdge(v3, v4); err != nil {
+		t.Fatalf("graph.AddEdge returned an error: %v", err)
+	}
+
+	sorted, err := graph.Sort()
+	if err != nil {
+		t.Fatalf("graph.Sort returned an error: %v", err)
+	}
+
+	// A DAG may have more than one possible topological sort
+	possibles := [][]string{
+		{"v5", "v1", "v3", "v2", "v4"},
+		{"v1", "v3", "v5", "v2", "v4"},
+		{"v3", "v5", "v1", "v4", "v2"},
+	}
+
+	var got []string
+	for _, vertex := range sorted {
+		got = append(got, vertex.Name)
+	}
+
+	if !isInPossibleSolutions(got, possibles) {
+		t.Errorf("DAG not sorted correctly: got:\n%#v\nwanted one of:\n%#v", got, possibles)
+	}
+}
+
+func TestSortNotADAG(t *testing.T) {
+	graph := New()
+
+	v1 := NewVertex("v1")
+	v2 := NewVertex("v2")
+	v3 := NewVertex("v3")
+	v4 := NewVertex("v4")
+	v5 := NewVertex("v5")
+
+	graph.AddVertex(v1)
+	graph.AddVertex(v2)
+	graph.AddVertex(v3)
+	graph.AddVertex(v4)
+	graph.AddVertex(v5)
+
+	// Purposely make it not a DAG (no vertices with in-degree of 0)
+	// easiest way is just connect everything to everything else so there's a cycle
+
+	// v2 depends on v1
+	if err := graph.AddEdge(v1, v2); err != nil {
+		t.Fatalf("graph.AddEdge returned an error: %v", err)
+	}
+
+	// v3 depends on v2
+	if err := graph.AddEdge(v2, v3); err != nil {
+		t.Fatalf("graph.AddEdge returned an error: %v", err)
+	}
+
+	// v4 depends on v3
+	if err := graph.AddEdge(v3, v4); err != nil {
+		t.Fatalf("graph.AddEdge returned an error: %v", err)
+	}
+
+	// v4 depends on v1
+	if err := graph.AddEdge(v1, v4); err != nil {
+		t.Fatalf("graph.AddEdge returned an error: %v", err)
+	}
+
+	// v5 depends on v4
+	if err := graph.AddEdge(v4, v5); err != nil {
+		t.Fatalf("graph.AddEdge returned an error: %v", err)
+	}
+
+	// Complete the cycle: v1 also depends on v5
+	if err := graph.AddEdge(v4, v1); err != nil {
+		t.Fatalf("graph.AddEdge returned an error: %v", err)
+	}
+
+	_, err := graph.Sort()
+	if err == nil {
+		t.Fatal("expected not a dag error, got nil")
+	}
+}
+
+func isInPossibleSolutions(result []string, possibles [][]string) bool {
+	for _, possible := range possibles {
+		if equal(result, possible) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func equal[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, item := range a {
+		if b[i] != item {
+			return false
+		}
+	}
+
+	return true
+}
+
+// makeGraph makes a simple DAG with a few connections for things like benchmarks.
+func makeGraph() *Graph {
+	graph := New()
+
+	v1 := NewVertex("v1")
+	v2 := NewVertex("v2")
+	v3 := NewVertex("v3")
+	v4 := NewVertex("v4")
+	v5 := NewVertex("v5")
+
+	graph.AddVertex(v1)
+	graph.AddVertex(v2)
+	graph.AddVertex(v3)
+	graph.AddVertex(v4)
+	graph.AddVertex(v5)
+
+	// v2 depends on v1
+	_ = graph.AddEdge(v1, v2)
+
+	// v4 depends on v3
+	_ = graph.AddEdge(v3, v4)
+
+	return graph
+}
+
+func BenchmarkGraphSort(b *testing.B) {
+	// Because the graph.Sort method alters the state of the graph (reducing vertex.InDegree)
+	// a new graph must be constructed for each run meaning this is actually quite slow to run (~1 minute)
+	// but we stop and start the timer at the right places to ensure just the sorting code's performance is measured
+	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+		graph := makeGraph()
+		b.StartTimer()
+		_, err := graph.Sort()
+		if err != nil {
+			b.Fatalf("graph.Sort returned an error: %v", err)
+		}
+	}
+}
