06e88ce5e0e0239ff7428c2274d56ff5807fe529
Tom Fleet <tomfleet2018@gmail.com>
2023-05-20 16:40:13 +0100

    Use msg.Error in main

--- cmd/spok/main.go
+++ cmd/spok/main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
+	"github.com/FollowTheProcess/msg"
 	"github.com/FollowTheProcess/spok/cli/cmd"
-	"github.com/fatih/color"
 )
 
 func main() {
 	rootCmd := cmd.BuildRootCmd()
 	if err := rootCmd.Execute(); err != nil {
-		color.New(color.FgHiRed, color.Bold).Fprint(os.Stderr, "✘ Error: ")
-		fmt.Fprintln(os.Stderr, err)
+		msg.Error("%s", err)
 		os.Exit(1)
 	}
 }
