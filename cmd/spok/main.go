8bb3f71109c363c0b3bf10c46885a8e7b099c3b8
Tom Fleet <tomfleet2018@gmail.com>
2024-09-09 14:55:11 +0100

    Use FollowTheProcess/cli for the CLI (#164)
    * Use my CLI library
    
    * Fix goreleaser deprecation
    
    * No longer set build by

--- cmd/spok/main.go
+++ cmd/spok/main.go
@@ -8,9 +8,16 @@ import (
 )
 
 func main() {
-	rootCmd := cmd.BuildRootCmd()
-	if err := rootCmd.Execute(); err != nil {
+	if err := run(); err != nil {
 		msg.Error("%s", err)
 		os.Exit(1)
 	}
+}
+
+func run() error {
+	rootCmd, err := cmd.BuildRootCmd()
+	if err != nil {
+		return err
+	}
+	return rootCmd.Execute()
 }
