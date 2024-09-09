7f311ab30e091979efe03eeaeb3caedcaabf83e6
Tom Fleet <tomfleet2018@gmail.com>
2024-09-09 14:50:17 +0100

    Use my CLI library

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -2,16 +2,16 @@
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/FollowTheProcess/cli"
 	"github.com/FollowTheProcess/spok/cli/app"
 	"github.com/FollowTheProcess/spok/iostream"
-	"github.com/spf13/cobra"
 )
 
 const (
 	short = "It's a build system Jim, but not as we know it!"
 	long  = `
-It's a build system Jim, but not as we know it!
-
 Spok is a lightweight build system and command runner, inspired by things like
 make, just etc.
 
@@ -23,61 +23,47 @@ However, spok offers a number of additional features such as:
 - Load .env files by default
 - Incremental runs based on file hashing and sum checks
 `
-	example = `
-# Spok prints all tasks by default
-$ spok
-
-# Run tasks named 'test' and 'lint'
-$ spok test lint
-
-# Show all defined variables in the 'spokfile'
-$ spok --vars
-
-# Format the spokfile
-$ spok --fmt`
 )
 
 var (
 	version   = "dev" // spok version, set at compile time by ldflags
 	commit    = ""    // spok version's commit hash, set at compile time by ldflags
 	buildDate = ""    // spok build date, set at compile time by ldflags
-	builtBy   = ""    // spok built by, set at compile time by ldflags
 )
 
 // BuildRootCmd builds and returns the root spok CLI command.
-func BuildRootCmd() *cobra.Command {
+func BuildRootCmd() (*cli.Command, error) {
 	spok := app.New(iostream.OS())
 
-	rootCmd := &cobra.Command{
-		Use:           "spok [tasks]...",
-		Version:       version,
-		Args:          cobra.ArbitraryArgs,
-		SilenceUsage:  true,
-		SilenceErrors: true,
-		Short:         short,
-		Long:          long,
-		Example:       example,
-		RunE: func(cmd *cobra.Command, args []string) error {
+	root, err := cli.New(
+		"spok",
+		cli.Short(short),
+		cli.Long(long),
+		cli.Example("Spok prints all tasks by default", "spok"),
+		cli.Example("Run tasks named 'test' and 'lint'", "spok test lint"),
+		cli.Example("Show all defined variables in the spokfile", "spok --vars"),
+		cli.Example("Format the spokfile", "spok --fmt"),
+		cli.Version(version),
+		cli.Commit(commit),
+		cli.BuildDate(buildDate),
+		cli.Flag(&spok.Options.Variables, "vars", cli.NoShortHand, false, "Show all defined variables in the spokfile"),
+		cli.Flag(&spok.Options.Fmt, "fmt", cli.NoShortHand, false, "Format the spokfile"),
+		cli.Flag(&spok.Options.Spokfile, "spokfile", cli.NoShortHand, "", "The path to the spokfile (defaults to '$CWD/spokfile')"),
+		cli.Flag(&spok.Options.Init, "init", cli.NoShortHand, false, "Initialise a new spokfile in $CWD"),
+		cli.Flag(&spok.Options.Clean, "clean", 'c', false, "Remove all build artifacts"),
+		cli.Flag(&spok.Options.Force, "force", 'f', false, "Bypass file hash checks and force requested tasks to run"),
+		cli.Flag(&spok.Options.Debug, "debug", cli.NoShortHand, false, "Show debug info"),
+		cli.Flag(&spok.Options.Quiet, "quiet", 'q', false, "Silence all CLI output."),
+		cli.Flag(&spok.Options.JSON, "json", 'j', false, "Output task results as JSON"),
+		cli.Flag(&spok.Options.Show, "show", 's', false, "Show all tasks defined in the spokfile"),
+		cli.Allow(cli.AnyArgs()),
+		cli.Run(func(cmd *cli.Command, args []string) error {
 			return spok.Run(args)
-		},
+		}),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("could not build spok CLI command: %w", err)
 	}
 
-	// Attach the flags
-	flags := rootCmd.Flags()
-	flags.BoolVarP(&spok.Options.Variables, "vars", "V", false, "Show all defined variables in spokfile.")
-	flags.BoolVar(&spok.Options.Fmt, "fmt", false, "Format the spokfile.")
-	flags.StringVar(&spok.Options.Spokfile, "spokfile", "", "The path to the spokfile (defaults to '$CWD/spokfile').")
-	flags.BoolVar(&spok.Options.Init, "init", false, "Initialise a new spokfile in $CWD.")
-	flags.BoolVarP(&spok.Options.Clean, "clean", "c", false, "Remove all build artifacts.")
-	flags.BoolVarP(&spok.Options.Force, "force", "f", false, "Bypass file hash checks and force running.")
-	flags.BoolVarP(&spok.Options.Debug, "debug", "d", false, "Show debug logging output.")
-	flags.BoolVarP(&spok.Options.Quiet, "quiet", "q", false, "Silence all CLI output.")
-	flags.BoolVarP(&spok.Options.JSON, "json", "j", false, "Output task results as JSON.")
-	flags.BoolVarP(&spok.Options.Show, "show", "s", false, "Show all tasks defined in the spokfile.")
-
-	// Set our custom version and usage templates
-	rootCmd.SetUsageTemplate(usageTemplate)
-	rootCmd.SetVersionTemplate(versionTemplate)
-
-	return rootCmd
+	return root, nil
 }
--- go.mod
+++ go.mod
@@ -1,32 +1,32 @@
 module github.com/FollowTheProcess/spok
 
-go 1.22
+go 1.23
+
+toolchain go1.23.1
 
 require (
+	github.com/FollowTheProcess/cli v0.4.0
 	github.com/FollowTheProcess/collections v0.5.2
-	github.com/FollowTheProcess/msg v1.1.0
+	github.com/FollowTheProcess/msg v1.2.0
 	github.com/bmatcuk/doublestar/v4 v4.6.1
 	github.com/fatih/color v1.17.0
 	github.com/google/go-cmp v0.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/juju/ansiterm v1.0.0
 	github.com/lithammer/fuzzysearch v1.1.8
-	github.com/spf13/cobra v1.8.1
 	go.uber.org/zap v1.27.0
-	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8
+	golang.org/x/exp v0.0.0-20240904232852-e7e105dedf7e
 	mvdan.cc/sh/v3 v3.9.0
 )
 
 require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lunixbochs/vtclean v1.0.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.23.0 // indirect
-	golang.org/x/term v0.23.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/term v0.24.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
